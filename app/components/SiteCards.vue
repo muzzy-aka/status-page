07919d6681d70997b642bd4a89b2f31421826c37
muzzy-aka <admin@muzzink.cn>
2025-02-27 18:15:17 +0800

    Update SiteCards.vue

--- app/components/SiteCards.vue
+++ app/components/SiteCards.vue
@@ -97,8 +97,10 @@
         </n-flex>
         <!-- 总结 -->
         <n-flex class="summary" justify="space-between">
+          <!--
           <n-text class="date" depth="1">
-            <!-- {{ formatTime(site?.days?.[0]?.date || 0) }} -->
+            {{ formatTime(site?.days?.[0]?.date || 0) }}
+          -->
           </n-text>
           <n-text v-if="site?.down?.times" depth="2">
             {{
@@ -117,7 +119,7 @@
               })
             }}
           </n-text>
-          <n-text class="date" depth="2"><!--{{ $t("meta.today") }}--></n-text>
+          <n-text class="date" depth="2">{{ $t("meta.today") }}</n-text>
         </n-flex>
       </n-card>
     </div>
